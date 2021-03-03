e3c9ca2fe52f33acd1c0394cb4bae1b0fa015a81
yeapes <yeapesh2831@gmail.com>
2021-03-03 23:24:57 +0600

    ProductCollection Modified

--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Resources\Product;
 
+use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
-class ProductCollection extends ResourceCollection
+class ProductCollection extends JsonResource
 {
     /**
      * Transform the resource collection into an array.
@@ -14,6 +15,14 @@ class ProductCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'price' => $this->price,
+            'totalPrice' => round(( 1 - ($this->discount/100)) * $this->price,2),
+            'rating' => $this->reviews->count() > 0? round($this->reviews->sum('star')/$this->reviews->count()) : 'No rating yet',
+            'discount' => $this->discount,
+            'href' => [
+                'link' => route('products.show',$this->id)
+            ]
+        ];
     }
 }
--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -21,7 +21,7 @@ class ProductResource extends JsonResource
             'stock' => $this->stock == 0? 'out of stock' : $this->stock,
             'discount' => $this->discount,
             'totalPrice' => round(( 1 - ($this->discount/100)) * $this->price,2),
-            'reviews' => $this->reviews->count() > 0? round($this->reviews->sum('star')/$this->reviews->count()) : 'No rating yet',
+            'rating' => $this->reviews->count() > 0? round($this->reviews->sum('star')/$this->reviews->count()) : 'No rating yet',
             'href' => [
                 'reviews' => route('reviews.index',$this->id)
             ]
